5452f5e9eb2b4820cffaf3ea18b25faadd0bee5a
AIT TR Student <luka.rus.gavric@gtmail.com>
2023-05-24 17:41:41 +0200

    feat: add Item details

--- src/Item.java
+++ src/Item.java
@@ -1,17 +1,19 @@
 import java.util.Date;
 
 public class Item {
-  class Item {
-    private String description;
-    public Item(String description) {
-      this.description = description;
-    }
-    public String getDescription() {
-      return description;
-    }
-    public void setDescription(String description) {
-      this.description = description;
-    }
-  }
+
   private Date date;
+  private String description;
+
+  public Item(String description) {
+    this.description = description;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
 }

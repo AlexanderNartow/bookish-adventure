be4285d44994e724ea7d81abbdb7dab73dcaae2a
AIT TR Student <luka.rus.gavric@gtmail.com>
2023-05-24 17:19:42 +0200

    feat: add method description

--- src/Item.java
+++ src/Item.java
@@ -1,6 +1,17 @@
 import java.util.Date;
 
 public class Item {
-
+  class Task {
+    private String description;
+    public Item(String description) {
+      this.description = description;
+    }
+    public String getDescription() {
+      return description;
+    }
+    public void setDescription(String description) {
+      this.description = description;
+    }
+  }
   private Date date;
 }

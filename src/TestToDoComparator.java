56ee42cef2406fe5bcbec093780830044e48966b
Alexander Nartow <Alexander.Nartow@gmx.de>
2023-05-25 13:07:57 -0700

    feat: add Abstract cases with dates

--- src/TestToDoComparator.java
+++ src/TestToDoComparator.java
@@ -1,6 +1,16 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import org.junit.jupiter.api.Test;
+
 class TestToDoComparator {
 
-  private static void TesToDoComparator() {
+  @Test
+
+  public void TestCompare() throws ParseException {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    Item item1 = new Item("Задача 1", dateFormat.parse("2023-05-30"));
+    Item item2 = new Item("Задача 2", dateFormat.parse("2023-05-25"));
+    Item item3 = new Item("Задача 3", dateFormat.parse("2023-06-02"));
 
   }
 }

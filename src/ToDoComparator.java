8a32117835b005c7b028401204a5f23d9a4af2cd
Alexander Nartow <Alexander.Nartow@gmx.de>
2023-05-25 10:42:21 -0700

    feat: add toDo comparator

---
+++ src/ToDoComparator.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+public class ToDoComparator implements compare {
+
+  @Override
+
+  public int compare(Item item1, Item item2) {
+    Date date1 = item1.getDate();
+    Date date2 = item2.getDate();
+    if (date1 == null && date2 == null) {
+      return 0;
+    } else if (date1 == null) {
+      return 1;
+    } else if (date2 == null) {
+      return -1;
+    }
+    return date1.compareTo(date2);
+  }
+
+}
--- src/ToDoList.java
+++ src/ToDoList.java
@@ -6,7 +6,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
@@ -86,20 +85,9 @@ class ToDoList {
   }
 
   public void sortItemsByDate() {
-    Collections.sort(items, new Comparator<Item>() {
-      @Override
-      public int compare(Item item1, Item item2) {
-        Date date1 = item1.getDate();
-        Date date2 = item2.getDate();
-        if (date1 == null && date2 == null) {
-          return 0;
-        } else if (date1 == null) {
-          return 1;
-        } else if (date2 == null) {
-          return -1;
-        }
-        return date1.compareTo(date2);
-      }
-    });
+    Collections.sort(items, new) {
+
+    }
+
   }
 }
